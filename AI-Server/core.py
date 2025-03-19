d13311416cee25e5f56337fec3f5e56ae2f7e743
Gowtham N K B <84592476+gowthamnkb@users.noreply.github.com>
2025-03-19 15:11:41 +0530

    added history

--- AI-Server/core.py
+++ AI-Server/core.py
@@ -1,5 +1,3 @@
-from agents import handoff
-
 from openaiagent.src.agents import Agent, Runner
 import openaiagent.src.agents as agents
 import asyncio
@@ -50,67 +48,3 @@ class ProcessModel:
         History.append({"role": "assistant", "content": response})
 
         return response
-
-
-"""
-1. Wallet Product
-1.1 Input Agent
-1.2 Txt Output Agent
-1.3. HTML Output Agent
-
-(optional if we have time)
-2. GCOMS Product
-2.1 Input Agent
-2.2 Txt Output Agent
-2.3. HTML Output Agent
-
-
--------------------------
-2 Agents
-1. Wallet Agent
-2. GC Agent
-
-
-
-Instrcution
-
-1. Input format
-1.1. point 1
-
-2. Text Output 
-"""
-
-    
-
-
-
-
-"""
-1. Wallet Product
-1.1 Input Agent
-1.2 Txt Output Agent
-1.3. HTML Output Agent
-
-
-Input1: what is the balance of wallet for contact xxx?
-Output1 : {"select * from balances where contact = xxx"}
-wallet : Rs. 1000
-Input2 : {"balance":1000}
-Output2 : "Wallet balance is Rs. 1000"
-
-
-
-I1: I want to do a load 
-O1: {"prompt":"Please provide the UserId"}
-I2: {"userId":"123"}
-O2: {"prompt":"select * from programs where user_id = '123"}
-I3: {"programId":"123"}
-O3: {"prompt":"After this load, balnce will be xx. Shall I proceed?"}
-I4: {"proceed":"yes"}
-O4: {"action":"CreateLoad", input}
-I5: {"Give out put of load respnose - otput}
-O5: {"Load is success. You recharge iD is xx, balance is yy}
-
-
-
-"""
