aea84a1d441ad49013754ac2bb82f2dadcdab731
Gowtham N K B <84592476+gowthamnkb@users.noreply.github.com>
2025-03-20 11:30:34 +0530

    use latest model

--- AI-Server/server.py
+++ AI-Server/server.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from core import ProcessModel
 
 
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route("/action", methods=["POST"])
 def health_check():
     data = request.json
-    return {"response":json.dumps(ProcessModel().process(data))}
+    return jsonify({"response":json.dumps(ProcessModel().process(data))})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5100, debug=True)
